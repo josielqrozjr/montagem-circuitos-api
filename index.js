728d1573536e735854730acc652a78a845e74b40
Queiroz Jr <josielqueirozjr@icloud.com>
2024-02-01 15:22:21 -0300

    update port 9001

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000; // Use a porta fornecida pelo Vercel
+const port = process.env.PORT || 9001; // Use a porta fornecida pelo Vercel
 
 const docsPdf = [
     {
